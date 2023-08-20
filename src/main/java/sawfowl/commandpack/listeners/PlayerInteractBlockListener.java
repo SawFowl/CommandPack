b23f439bc256b208de1a660b9616f24fc5e5665e
Иван <StalkerIv@yandex.ru>
2023-08-20 18:40:35 +0300

    Update PlayerInteractBlockListener.java

--- src/main/java/sawfowl/commandpack/listeners/PlayerInteractBlockListener.java
+++ src/main/java/sawfowl/commandpack/listeners/PlayerInteractBlockListener.java
@@ -5,7 +5,6 @@ import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.InteractBlockEvent;
 import org.spongepowered.api.event.filter.cause.First;
 
-import net.kyori.adventure.text.Component;
 import sawfowl.commandpack.CommandPack;
 
 public class PlayerInteractBlockListener {
@@ -18,10 +17,6 @@ public class PlayerInteractBlockListener {
 	@Listener
 	public void onInteract(InteractBlockEvent event, @First ServerPlayer player) {
 		if(plugin.getMainConfig().getAfkConfig().isEnable()) plugin.getPlayersData().getTempData().updateLastActivity(player);
-		player.sendMessage(Component.text(plugin.getEconomy().getCurrencies().size()));
-		plugin.getEconomy().getCurrencies().forEach(currency -> {
-			player.sendMessage(currency.displayName().append(Component.text(plugin.getEconomy().getPlayerBalance(player.uniqueId(), currency).doubleValue())));
-		});
 	}
 
 }
