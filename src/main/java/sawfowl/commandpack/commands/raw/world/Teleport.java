9fee931b88df09731191a256850bc383046fb955
Иван <StalkerIv@yandex.ru>
2023-08-21 15:39:28 +0300

    Update Teleport.java

--- src/main/java/sawfowl/commandpack/commands/raw/world/Teleport.java
+++ src/main/java/sawfowl/commandpack/commands/raw/world/Teleport.java
@@ -89,7 +89,7 @@ public class Teleport extends AbstractWorldCommand {
 	public List<RawArgument<?>> arguments() {
 		return Arrays.asList(
 			createWorldArg(),
-			RawArguments.createPlayerArgument(false, false, 1, LocalesPaths.COMMANDS_EXCEPTION_PLAYER_NOT_PRESENT)
+			RawArguments.createPlayerArgument(true, false, 1, LocalesPaths.COMMANDS_EXCEPTION_PLAYER_NOT_PRESENT)
 		);
 	}
 
