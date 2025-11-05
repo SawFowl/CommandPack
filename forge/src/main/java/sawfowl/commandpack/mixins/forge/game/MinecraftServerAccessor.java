bf7b825ebfa8eaf4a672272c44b274ba5d63dec6
Иван <StalkerIv@yandex.ru>
2025-11-05 13:22:08 +0300

    Implementation of the network API for Forge.

---
+++ forge/src/main/java/sawfowl/commandpack/mixins/forge/game/MinecraftServerAccessor.java
@@ -0,0 +1,18 @@
+package sawfowl.commandpack.mixins.forge.game;
+
+import org.jetbrains.annotations.Contract;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerConnectionListener;
+
+@Mixin(MinecraftServer.class)
+public interface MinecraftServerAccessor {
+
+	@Accessor
+	@Contract("-> _")
+	static ServerConnectionListener getconnection() {
+		throw new AssertionError();
+	}
+}
