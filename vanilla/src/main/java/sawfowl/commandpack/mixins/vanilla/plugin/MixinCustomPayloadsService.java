cd080f32898b3c3b1396dc785b31e4fbe36afb02
Иван <StalkerIv@yandex.ru>
2025-11-07 12:57:07 +0300

    Update MixinCustomPayloadsService.java

--- vanilla/src/main/java/sawfowl/commandpack/mixins/vanilla/plugin/MixinCustomPayloadsService.java
+++ vanilla/src/main/java/sawfowl/commandpack/mixins/vanilla/plugin/MixinCustomPayloadsService.java
@@ -48,7 +48,7 @@ public abstract class MixinCustomPayloadsService {
 	}
 
 	@Overwrite
-	public void registerRawCodecAndChannel(ResourceKey channel) {
+	public void registerChannel(ResourceKey channel) {
 		if(finished) {
 			plugin.getLocales().getSystemLocale().getDebug().getFinishedRegisterNetworkData(channel);
 		} else if(!spongeChannelsToRegister.contains(channel)) spongeChannelsToRegister.add(channel);
